2631b16ae4f5ef78fb96607632228472aa199428
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2017-10-02 12:58:46 +0200

    Update DataSet.py

--- DataSet.py
+++ DataSet.py
@@ -8,27 +8,44 @@
 ###########################
 ###########################
 
+#########################
+# 	SUMMARY
+#########################
 
 #########################
 # 	IMPORT
 #########################
 
-
 from StockData import *
-from StockResume import * 
+from StockResume import *
+from FolderManager import * 
+
+
+#########################
+# 	VAR & CONST
+#########################
+
+DEFAULT_FILE_LIST = ['AC.csv', 'ACA.csv', 'AI.csv', 'AIR.csv', 'ATO.csv', 'BN.csv', 
+'BNP.csv', 'CA.csv', 'CAP.csv', 'CS.csv', 'DG.csv', 'EI.csv', 'EN.csv', 'ENGI.csv', 
+'FP.csv', 'FR.csv', 'FTI.csv', 'GLE.csv', 'KER.csv', 'LHN.csv', 'LR.csv', 'MC.csv', 
+'ML.csv', 'MT.csv', 'OR.csv', 'ORA.csv', 'PUB.csv', 'RI.csv', 'RNO.csv', 'SAF.csv',
+'SAN.csv', 'SGO.csv', 'SOLB.csv', 'STM.csv', 'SU.csv', 'SW.csv', 'UG.csv', 'UL.csv',
+'VIE.csv', 'VIV.csv']
+
+
+DEFAULT_REP = "/home/alex/stock_exchange_project/v1.1.1/Quotes/CAC40"
 
 
 #########################
 # 	CLASSES
 #########################
 
-
 class DataSet(): 
 	"""
 	blabla
 	"""
 
-	def __init__(self, file_list) : 
+	def __init__(self, file_list=DEFAULT_FILE_LIST, rep=DEFAULT_REP) : 
 		"""
 		blabla
 		"""
@@ -37,43 +54,80 @@ class DataSet():
 
 		#creating the dataset of Data objects
 		for fi in file_list : 
-			dataset.append(Data(fi))
+			dataset.append(StockData(fi, rep))
 
 		for i, _ in enumerate(dataset) :
-			dataset[i].extract_file()
-			dataset[i].manage_raw_data()
-			dataset[i].add_MMA30_MMA50()
-			dataset[i].add_price_MMA30()
-			dataset[i].add_KPI()
-			dataset[i].add_volatility()
-			dataset[i].reduce_data()
+			dataset[i].upgrade()
 
 		self.object_list = dataset
 		self.size = len(self.object_list)
+		self.mean = dict()
 
 
-	def add_summary(self): 
-		self.summary = Data(file="Mean", source="None")
-		self.summary.reduce_data()
+	def resume_stock(self):
+		"""
+		blabla
+		"""
 
-		for obj in self.object_list : 
-			for i,j in obj.KPI.items() : 
-				if i in list(self.summary.KPI.keys()) : self.summary.KPI[i] +=j
-				else : self.summary.KPI[i] = j
-              
-		for i, j in self.summary.KPI.items(): 
-			self.summary.KPI[i] = round(j/self.size, DECIMAL_LEVEL)
-               
+		for i, _ in enumerate(self.object_list) :
+			self.object_list[i] = StockResume(self.object_list[i])
 
-		for obj in self.object_list : 
-			for i,j in obj.volatility.items() : 
-				if i in list(self.summary.volatility.keys()) : self.summary.volatility[i] +=j
-				else : self.summary.volatility[i] = j
 
-		for i, j in self.summary.volatility.items(): 
-			self.summary.volatility[i] = round(j/self.size ,DECIMAL_LEVEL)
+	def add_mean(self) : 
+		"""
+		blabla
+		"""
 
-		self.summary.name = "Mean"
+		mean_ = dict()
+		
+		# KPI
+		mean_["KPI"] = dict()
+		mean_["KPI"]["buy_begin_sell_end"] = 0
+		mean_["KPI"]["global_max_possible"] = 0
+
+		for i,j in enumerate(self.object_list) :
+			mean_["KPI"]["buy_begin_sell_end"] += j.KPI["buy_begin_sell_end"]
+			mean_["KPI"]["global_max_possible"] += j.KPI["global_max_possible"]
+
+		mean_["KPI"]["buy_begin_sell_end"] = round(mean_["KPI"]["buy_begin_sell_end"] / self.size, DECIMAL_LEVEL)
+		mean_["KPI"]["global_max_possible"] = round(mean_["KPI"]["global_max_possible"] / self.size, DECIMAL_LEVEL)
+
+		self.mean["KPI"] = mean_["KPI"]	
+
+		# price Volatility
+		mean_["price_volatility"] = dict()
+		mean_["price_volatility"]["log_volatility"] = 0
+		mean_["price_volatility"]["5_days_volatility"] = 0
+		mean_["price_volatility"]["min_max"] = 0
+
+		for i,j in enumerate(self.object_list) :
+			mean_["price_volatility"]["log_volatility"] += j.price_volatility["log_volatility"]
+			mean_["price_volatility"]["5_days_volatility"] += j.price_volatility["5_days_volatility"]
+			mean_["price_volatility"]["min_max"] += j.price_volatility["min_max"]
+
+		mean_["price_volatility"]["log_volatility"] = round(mean_["price_volatility"]["log_volatility"] /self.size, DECIMAL_LEVEL)
+		mean_["price_volatility"]["5_days_volatility"]  = round(mean_["price_volatility"]["5_days_volatility"] / self.size, DECIMAL_LEVEL)
+		mean_["price_volatility"]["min_max"] = round(mean_["price_volatility"]["min_max"] / self.size, DECIMAL_LEVEL)
+
+		self.mean["price_volatility"] = mean_["price_volatility"]
+
+
+		# volume Volatility
+		mean_["volume_volatility"] = dict()
+		mean_["volume_volatility"]["log_volatility"] = 0
+		mean_["volume_volatility"]["5_days_volatility"] = 0
+		mean_["volume_volatility"]["min_max"] = 0
+
+		for i,j in enumerate(self.object_list) :
+			mean_["volume_volatility"]["log_volatility"] += j.volume_volatility["log_volatility"]
+			mean_["volume_volatility"]["5_days_volatility"] += j.price_volatility["5_days_volatility"]
+			mean_["volume_volatility"]["min_max"] += j.price_volatility["min_max"]
+
+		mean_["volume_volatility"]["log_volatility"] = round(mean_["volume_volatility"]["log_volatility"] /self.size, DECIMAL_LEVEL)
+		mean_["volume_volatility"]["5_days_volatility"] = round(mean_["volume_volatility"]["5_days_volatility"] / self.size, DECIMAL_LEVEL)
+		mean_["volume_volatility"]["min_max"] = round(mean_["volume_volatility"]["min_max"] / self.size, DECIMAL_LEVEL)
+
+		self.mean["volume_volatility"] = mean_["volume_volatility"]
 
 
 	def __repr__(self) : 
@@ -86,15 +140,9 @@ class DataSet():
 		return string
 
 
-	def __str__(self) : 
-		"""
-		blabla
-		"""
-
-		string = "objet : {}, \nsize : {}, \nKPI : {}, \nvolatility : {} \n".format(
-			self.name, self.size, self.KPI, self.volatility)
-		return string
-
+#########################
+# 	FUNCTIONS
+#########################
 
 
 #########################
@@ -102,7 +150,12 @@ class DataSet():
 #########################
 
 
-
 if __name__ == '__main__':
+    dataset = DataSet()
+    dataset.resume_stock()
+    dataset.add_mean()
 
-  pass
+    datasource = FolderManager()
+
+    dataset2 = DataSet(file_list=datasource.source_files, rep=datasource.source_folder)
+    dataset2.add_mean()
