3c81d08f4ee027e7a94f7c93c3a1383bbc59b5ca
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2017-10-03 01:34:35 +0200

    Update DataSet.py

--- DataSet.py
+++ DataSet.py
@@ -8,9 +8,11 @@
 ###########################
 ###########################
 
+
 #########################
 # 	SUMMARY
 #########################
+
 
 #########################
 # 	IMPORT
@@ -25,13 +27,14 @@ from FolderManager import *
 # 	VAR & CONST
 #########################
 
-DEFAULT_FILE_LIST = ['AC.csv', 'ACA.csv', 'AI.csv', 'AIR.csv', 'ATO.csv', 'BN.csv', 
+DEFAULT_FILE_LIST = ['AC.csv', 'ACA.csv']
+""", 'AI.csv', 'AIR.csv', 'ATO.csv', 'BN.csv', 
 'BNP.csv', 'CA.csv', 'CAP.csv', 'CS.csv', 'DG.csv', 'EI.csv', 'EN.csv', 'ENGI.csv', 
 'FP.csv', 'FR.csv', 'FTI.csv', 'GLE.csv', 'KER.csv', 'LHN.csv', 'LR.csv', 'MC.csv', 
 'ML.csv', 'MT.csv', 'OR.csv', 'ORA.csv', 'PUB.csv', 'RI.csv', 'RNO.csv', 'SAF.csv',
 'SAN.csv', 'SGO.csv', 'SOLB.csv', 'STM.csv', 'SU.csv', 'SW.csv', 'UG.csv', 'UL.csv',
 'VIE.csv', 'VIV.csv']
-
+"""
 
 DEFAULT_REP = "/home/alex/stock_exchange_project/v1.1.1/Quotes/CAC40"
 
@@ -61,7 +64,8 @@ class DataSet():
 
 		self.object_list = dataset
 		self.size = len(self.object_list)
-		self.mean = dict()
+		
+		self.add_mean()
 
 
 	def resume_stock(self):
@@ -78,56 +82,24 @@ class DataSet():
 		blabla
 		"""
 
-		mean_ = dict()
-		
-		# KPI
-		mean_["KPI"] = dict()
-		mean_["KPI"]["buy_begin_sell_end"] = 0
-		mean_["KPI"]["global_max_possible"] = 0
-
-		for i,j in enumerate(self.object_list) :
-			mean_["KPI"]["buy_begin_sell_end"] += j.KPI["buy_begin_sell_end"]
-			mean_["KPI"]["global_max_possible"] += j.KPI["global_max_possible"]
-
-		mean_["KPI"]["buy_begin_sell_end"] = round(mean_["KPI"]["buy_begin_sell_end"] / self.size, DECIMAL_LEVEL)
-		mean_["KPI"]["global_max_possible"] = round(mean_["KPI"]["global_max_possible"] / self.size, DECIMAL_LEVEL)
-
-		self.mean["KPI"] = mean_["KPI"]	
+		# KPI_
+		self.KPI = {elem : 0 for elem in self.object_list[0].KPI.keys()}
+		for elem in self.KPI.keys() : 
+			for i,j in enumerate(self.object_list) : self.KPI[elem] += j.KPI[elem]
+		for elem in self.KPI.keys() : self.KPI[elem] = round(self.KPI[elem] / self.size, DECIMAL_LEVEL)
 
 		# price Volatility
-		mean_["price_volatility"] = dict()
-		mean_["price_volatility"]["log_volatility"] = 0
-		mean_["price_volatility"]["5_days_volatility"] = 0
-		mean_["price_volatility"]["min_max"] = 0
-
-		for i,j in enumerate(self.object_list) :
-			mean_["price_volatility"]["log_volatility"] += j.price_volatility["log_volatility"]
-			mean_["price_volatility"]["5_days_volatility"] += j.price_volatility["5_days_volatility"]
-			mean_["price_volatility"]["min_max"] += j.price_volatility["min_max"]
-
-		mean_["price_volatility"]["log_volatility"] = round(mean_["price_volatility"]["log_volatility"] /self.size, DECIMAL_LEVEL)
-		mean_["price_volatility"]["5_days_volatility"]  = round(mean_["price_volatility"]["5_days_volatility"] / self.size, DECIMAL_LEVEL)
-		mean_["price_volatility"]["min_max"] = round(mean_["price_volatility"]["min_max"] / self.size, DECIMAL_LEVEL)
-
-		self.mean["price_volatility"] = mean_["price_volatility"]
-
+		self.price_volatility = {elem : 0 for elem in self.object_list[0].price_volatility.keys()}
+		for elem in self.price_volatility.keys() : 
+			for i,j in enumerate(self.object_list) : self.price_volatility[elem] += j.price_volatility[elem]
+		for elem in self.price_volatility.keys() : self.price_volatility[elem] = round(self.price_volatility[elem] /self.size, DECIMAL_LEVEL)
 
 		# volume Volatility
-		mean_["volume_volatility"] = dict()
-		mean_["volume_volatility"]["log_volatility"] = 0
-		mean_["volume_volatility"]["5_days_volatility"] = 0
-		mean_["volume_volatility"]["min_max"] = 0
+		self.volume_volatility = {elem : 0 for elem in self.object_list[0].volume_volatility.keys()}
+		for elem in self.volume_volatility.keys() : 
+			for i,j in enumerate(self.object_list) : self.volume_volatility[elem] += j.volume_volatility[elem]
+		for elem in self.volume_volatility.keys() : self.volume_volatility[elem] = round(self.volume_volatility[elem] /self.size, DECIMAL_LEVEL)
 
-		for i,j in enumerate(self.object_list) :
-			mean_["volume_volatility"]["log_volatility"] += j.volume_volatility["log_volatility"]
-			mean_["volume_volatility"]["5_days_volatility"] += j.price_volatility["5_days_volatility"]
-			mean_["volume_volatility"]["min_max"] += j.price_volatility["min_max"]
-
-		mean_["volume_volatility"]["log_volatility"] = round(mean_["volume_volatility"]["log_volatility"] /self.size, DECIMAL_LEVEL)
-		mean_["volume_volatility"]["5_days_volatility"] = round(mean_["volume_volatility"]["5_days_volatility"] / self.size, DECIMAL_LEVEL)
-		mean_["volume_volatility"]["min_max"] = round(mean_["volume_volatility"]["min_max"] / self.size, DECIMAL_LEVEL)
-
-		self.mean["volume_volatility"] = mean_["volume_volatility"]
 
 
 	def __repr__(self) : 
@@ -151,11 +123,9 @@ class DataSet():
 
 
 if __name__ == '__main__':
+    
     dataset = DataSet()
     dataset.resume_stock()
-    dataset.add_mean()
 
     datasource = FolderManager()
-
     dataset2 = DataSet(file_list=datasource.source_files, rep=datasource.source_folder)
-    dataset2.add_mean()
