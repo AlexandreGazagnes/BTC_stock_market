4f4631a1f6d27a33b364cf2ee724714ffdbc680b
Alexandre GAZAGNES <a_syoez@yahoo.fr>
2017-10-02 12:59:26 +0200

    Update FolderManager.py

--- FolderManager.py
+++ FolderManager.py
@@ -20,6 +20,8 @@
 # 	IMPORT
 #########################
 
+import os 
+
 
 #########################
 # 	VAR & CONST
@@ -31,28 +33,52 @@
 #########################
 
 
+class FolderManager : 
+	"""
+	blablabla
+	"""
+
+
+	def ask_source_folder(self) : 
+		"""
+		blabla
+		"""
+	
+		self.source_folder = "/home/alex/stock_exchange_project/v1.1.1/Quotes/CAC40"
+
+
+	def ask_source_files(self) : 
+		"""
+		blabla
+		"""
+
+		os.chdir(self.source_folder)
+		self.source_files = os.listdir()
+		os.chdir(self.init_folder)
+
+
+	def __init__(self):
+		"""
+		blabla
+		"""
+
+		self.init_folder = os.getcwd()
+		self.source_folder = str()
+		self.dest_folder = str()
+		self.source_files = list()
+		self.dest_files = list()
+
+		self.ask_source_folder()
+		self.ask_source_files()
+
+
+
+
 #########################
 # 	FUNCTIONS
 #########################
 
 
-def ask_folder() : 
-	"""
-	blabla
-	"""
-	
-	return "/home/alex/stock_exchange_project/v1.1.0/Quotes/CAC40/"
-
-
-def ask_files(folder) : 
-	"""
-	blabla
-	"""
-
-	os.chdir(folder)
-	files = os.listdir()
-
-	return files
 
 
 
@@ -62,4 +88,4 @@ def ask_files(folder) :
 
 
 if __name__ == '__main__':
-	pass
+	datasource = FolderManager()
